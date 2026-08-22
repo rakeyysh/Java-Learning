3d1e96b6b09a63589ee2bcc4bf33342057289e4f
rakeyysh <8349733408a@gmail.com>
2026-08-22 20:15:27 +0530

    Initial Java learning setup

---
+++ Demo.java
@@ -0,0 +1,38 @@
+public class Demo {
+
+    public static void main(String[] args) {
+
+        // Integers - short, byte , int , long
+
+        byte a = 5;
+
+        short b = 1200;
+
+        long c = 10000000;
+
+        // Real Values
+
+        float d = 5.6f; // single precision
+        double f = 6.72; // double precision. --> standard way
+
+        // Characters
+
+        char x = 'X';
+
+        char y = 'B';
+
+        // boolean
+
+        boolean digit = false;
+
+        System.out.println("Integers" + "byte" + a + "," + "short" + b + "," + "long" + c);
+
+        System.out.println("Reals" + "float" + d + "," + "double" + f);
+
+        System.out.println("Characters" + "x" + "=" + x + "&" + "b" + b);
+
+        System.out.println("boolean" + "digits" + digit);
+
+    }
+
+};
