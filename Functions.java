097986cbd4a775603c36e1bf7b056570ba8c833b
rakeyysh <8349733408a@gmail.com>
2026-08-25 00:22:43 +0530

    Function overloading

---
+++ Functions.java
@@ -0,0 +1,42 @@
+public class Functions {
+
+    public static void main(String[] args) {
+
+        int result;
+        double res;
+        result = add(1, 2);
+
+        // result = add(5,8,10);
+
+        res = add(5.76, 9.97);
+
+        System.out.println("Result is " + result);
+        System.out.println("Res is " + res);
+
+    }
+
+    static int add(int a, int b) {
+
+        return a + b;
+    }
+
+    static int add(int x, int y, int z) {
+        return x + y + z;
+    }
+
+    static double add(double p, double q) {
+        return p + q;
+    }
+
+    // This Gives Error as it decides from the left side during compile time before
+    // execution
+    // The return type of the receiving variable cannot be used to select an
+    // overloaded method. Overload resolution happens at compile time based
+    // primarily on the method invocation and its argument types, before the method
+    // executes
+
+    // static double add (int p, int q){
+    // return (double)(p+q);
+    // }
+
+}
