18c8ff3c2bef95299e631f3bf8136913fab0ff7f
rakeyysh <8349733408a@gmail.com>
2026-08-27 14:24:39 +0530

    Inheritence and super

---
+++ Encapsulation.java
@@ -0,0 +1,28 @@
+public class Encapsulation {
+
+    public static void main(String[] args) {
+
+        BankAccount A = new BankAccount();
+        A.deposit(100, 280);
+        System.out.println(A.getBalance());
+
+    }
+}
+
+class BankAccount {
+
+    private int balance;
+    private int accountNumber;
+
+    public void deposit(int balance, int accountNumber) {
+        this.balance += balance;
+        this.accountNumber += accountNumber;
+    }
+
+    public double getBalance() {
+
+        return this.balance;
+
+    }
+
+}
---
+++ Inheritence.java
@@ -0,0 +1,64 @@
+public class Inheritence {
+
+    public static void main(String[] args) {
+
+        // parent Class object
+
+        EngineeringStudent A = new EngineeringStudent(); // Child class object
+
+        A.branch = "Basic Mechanical Engineering";
+
+        // A.attendLab();
+        // A.attendClass();
+
+        // Student B = new Student();
+        // B.name = "Raju";
+        // B.attendClass();
+
+        A.attendLab();
+
+    }
+
+}
+
+class Student {
+
+    public String name;
+    public int age;
+    public int height;
+    public int weight;
+
+    Student(String name, int age, int height, int weight) {
+
+        this.name = name;
+        this.age = age;
+        this.height = height;
+        this.weight = weight;
+
+    }
+
+    public void attendClass() {
+
+        System.out.println(this.name + " attended the class today");
+    }
+
+}
+
+class EngineeringStudent extends Student {
+
+    public String name;
+
+    public String branch;
+
+    EngineeringStudent() {
+        super("Rakesh", 28, 170, 72);
+    }
+
+    public void attendLab() {
+
+        System.out.println("name " + super.name + " age " + this.age + " height " + this.height + " weight "
+                + this.weight + " branch " + this.branch);
+        System.out.println(super.name + " attended the lab today of " + this.branch);
+    }
+
+}
